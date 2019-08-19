00d01f5f903796a40ec496ed19fe4e515d552af6
SergeyGolenko <1989sergiotay@gmail.com>
2019-08-19 13:12:10 +0300

    addButtons

--- UIButton/ViewController.swift
+++ UIButton/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    var button4 = UIButton()
 
+    @IBAction func Button(_ sender: Any) {
+        let button = sender as! UIButton
+        if button.titleLabel?.text == "Button 1"{
+            print("Это первая кнопка")
+            }
+        if button.titleLabel?.text == "Button 2"{
+            print("Это вторая кнопка")
+        } else if button.titleLabel?.text == "Button 3"{
+            print("Это третья кнопка")
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       button4 = UIButton(type:.infoLight)
+        button4.frame = CGRect(x: 110, y: 200, width: 100, height: 44)
+         button4.setTitle("Button4", for: .normal)
+        button4.setTitle("Ты нажал на меня", for: .highlighted )
+        button4.titleLabel?.text = "Button4"
+        self.view.addSubview(button4)
+        button4.addTarget(self, action:  #selector(buttonPressef(param:)), for:.touchUpInside)
     }
 
+    @objc func buttonPressef(param:UIButton){
+        print("Кнопка нажата")
+    }
 
 }
 
